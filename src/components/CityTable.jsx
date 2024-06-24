740c359ba18e2971710132cad935bd1ca1e9cb46
Yehtetaung158 <yha15380@gmail.com>
2024-06-24 21:58:10 +0630

    Add page is well done

---
+++ src/components/CityTable.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+// import swal from 'sweetalert';
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+const CityTable = ({ citydata, deletefun }) => {
+  console.log(citydata);
+  const handleDelete = () => {
+    MySwal.fire({
+        html: '<p>Are your sure to delete</p>',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#8796f9',
+        confirmButtonText: 'delete it!',
+        cancelButtonText: 'cancel',
+        customClass: {
+            text:'text-sm font-bold',
+          title: 'text-2xl font-bold ',
+          popup: 'custom-popup',
+        },
+        background: '#f3f4f4',
+        backdrop: `
+          transparent
+        `
+      }).then((result) => {
+      if (result.isConfirmed) {
+        deletefun();
+        MySwal.fire("Deleted!", "Your city has been deleted.", "success");
+      }
+    });
+  };
+  return (
+    <>
+      {citydata?.map((i, index) => (
+        <tr key={index} className="bg-white  divide-y divide-gray-200">
+          <td className="px-2 py-4 whitespace-nowrap text-sm font-medium text-gray-900 sm:px-4 md:px-6">
+            {i.city_name}
+          </td>
+          <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500 sm:px-4 md:px-6">
+            <img
+              className=" w-10"
+              src={`https://cdn.weatherbit.io/static/img/icons/${i.weather.icon}.png`}
+              // alt=""
+            />
+          </td>
+          <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500 sm:px-4 md:px-6 text-center">
+            {i.temp}° C
+          </td>
+          <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500 sm:px-4 md:px-6 text-center">
+            <button onClick={handleDelete}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-4 text-red-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                />
+              </svg>
+            </button>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
+
+export default CityTable;
--- src/page/Add.jsx
+++ src/page/Add.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useGetCurrentByCityMutation } from "../store/service/endpiont/weatherendpoint";
 import { useNavigate } from "react-router-dom";
+import CityTable from "../components/CityTable";
+import Swal from "sweetalert2";
 
 const Add = () => {
   const [currentfun, { isError, isLoading, data }] =
     useGetCurrentByCityMutation();
+
+    function showAlert() {
+      Swal.fire({
+        text: 'Require correct input',
+        confirmButtonColor: '#d33',
+        background: '#f3f4f4',
+      })}
+
   const currentData = data?.data[0];
   const nav = useNavigate();
   const [input, setInput] = useState("");
@@ -27,13 +37,13 @@ const Add = () => {
   const citydata = JSON.parse(localStorage.getItem("myArray"));
   const deletefun = (deleteCity) => {
     const currentCity = citydata.findIndex((i) => deleteCity === i.city_name);
-    console.log(currentCity);
+    // console.log(currentCity);
     citydata.splice(currentCity, 1);
     localStorage.setItem("myArray", JSON.stringify(citydata));
     setRs(!rs);
     setInput("");
   };
-
+  console.log(citydata.length);
   useEffect(() => {
     if (currentData) {
       addObjectToLocalStorage(currentData);
@@ -68,6 +78,7 @@ const Add = () => {
           ADD
         </button>
       </div>
+      {isError && showAlert() }
       <div className="overflow-x-auto sm:mx-2 sm:my-2 md:mx-4 md:my-4 lg:mx-6 lg:my-6 xl:mx-8 xl:my-8 2xl:mx-10 2xl:my-10 sm:px-2 md:px-4 lg:px-6 xl:px-8 2xl:px-10">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
@@ -86,44 +97,45 @@ const Add = () => {
               </th>
             </tr>
           </thead>
-          {citydata?.map((i, index) => (
-            <tbody key={index} className="bg-white divide-y divide-gray-200">
-              <tr>
-                <td className="px-2 py-4 whitespace-nowrap text-sm font-medium text-gray-900 sm:px-4 md:px-6">
-                  {i.city_name}
-                </td>
-                <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500 sm:px-4 md:px-6">
+          <tbody>
+            {!isLoading && (
+              <tr className={`${citydata.length === 0 ? "" : "hidden"} `}>
+                <td
+                  colSpan={4}
+                  className={`table-cell-4 text-center text-gray-600 w-full m-auto`}
+                >
                   <img
-                    className=" w-10"
-                    src={`https://cdn.weatherbit.io/static/img/icons/${i.weather.icon}.png`}
-                    // alt=""
+                    className="w-[200px] m-auto"
+                    src="img/Empty street-rafiki.svg"
+                    alt=""
                   />
-                </td>
-                <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500 sm:px-4 md:px-6 text-center">
-                  {i.temp}° C
-                </td>
-                <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500 sm:px-4 md:px-6 text-center">
-                  <button onClick={deletefun}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-4 text-red-700"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button>
+                  <p>There is no List</p>
                 </td>
               </tr>
-              {/* More rows... */}
-            </tbody>
-          ))}
+            )}
+            <>
+              {isLoading ? (
+                <tr>
+                  <td
+                    colSpan={4}
+                    className="px-2 py-4 whitespace-nowrap text-sm font-medium text-gray-900 sm:px-4 md:px-6"
+                  >
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+                        <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+                      </div>
+                      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+                    </div>
+                  </td>
+                </tr>
+              ) : (
+                <>
+                  <CityTable citydata={citydata} deletefun={deletefun} />
+                </>
+              )}
+            </>
+          </tbody>
         </table>
       </div>
     </div>
